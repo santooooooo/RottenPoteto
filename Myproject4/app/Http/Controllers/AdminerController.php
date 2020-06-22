9f6a1e46fc9d343ffcb75fcc2580db783032e1c0
santooooooo <santo.shunsuke@gmail.com>
2020-06-22 12:26:20 +0900

    changed some codes in app/Http/Controller/AdminerController

--- Myproject4/app/Http/Controllers/AdminerController.php
+++ Myproject4/app/Http/Controllers/AdminerController.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Adminer\CheckAdminer;
 use App\Http\Requests\AdminerAuthForm;
-use Illuminate\Http\Request;
 use Illuminate\View\View; 
 use Illuminate\Http\RedirectResponse;
-use App\Model\Adminer\ChangeSafety;
-use App\Model\Adminer\UsersData;
 
 class AdminerController extends Controller
 {
 		/**
-		 * @var string $email
+		 * @var string $gmail
 		 * @var string $password
 		 * @var bool $auth
 		 * @return View | RedirectResponse
 		 */
     public function adminerAuth(AdminerAuthForm $request)
     {
-	    $email = $request->input('gmail');
+	    $gmail = $request->input('gmail');
 	    $password = $request->input('password');
 
-	    $auth = CheckAdminer::check($email) && $password == 'fjhvhervheusheuhjihmnv,klsheish@p-9-0';
+	    $auth = $gmail == 'santo.shunsuke@gmail.com' &&
+		   			 $password == 'fjhvhervheusheuhjihmnv,klsheish@p-9-0';
 
 	    if($auth)
 	    {
@@ -31,27 +28,5 @@ class AdminerController extends Controller
 	    }
 
 	    return redirect('/adminer')->with('message', '管理者として認証できませんでした。');
-    }
-
-    /**
-     * @var string $email
-     * @return RedirectResponse
-     */
-    public function controllUser(Request $request)
-    {
-	    $email = $request->input('gmail');
-	    ChangeSafety::change($email);
-
-	    return redirect('adminer')->with('message', 'アカウントの設定変更が完了しました。');
-    }
-
-    /**
-     * @var string $usersData
-     */
-    public function getUsersData(): string
-    {
-	    $usersData = UsersData::jsonData();
-
-	    return $usersData;
     }
 }
